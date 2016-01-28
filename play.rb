298004e5215577f0761de237c4e2516f16429cfd
Petro Koval <kovalpetro@outlook.com>
2016-01-28 10:18:27 +0200

    Add playground snapshots

--- play.rb
+++ play.rb
@@ -5,14 +5,19 @@ size = [4, 4]
 seed = [[1, 1], [1, 2], [2, 1], [4, 4], [3, 4], [4, 3]]
 puts "Enter number of iterations"
 iter = gets.chomp.to_i
-
+playground_snapshots = []
 playground = Playground.new(size, seed)
+playground_snapshots << Marshal.dump(playground)
 
 iter.times do |i|
   UnderPopulationRule.use(playground)
   OverPopulationRule.use(playground)
   BornRule.use(playground)
   playground = NextIteration.save(playground)
-  puts "---------------Iteration #{i + 1}---------------------"
-  playground.cells.each { |cell| puts "[#{cell.x}, #{cell.y}], alive = #{cell.alive}" }
+  playground_snapshots << Marshal.dump(playground)
+end
+
+playground_snapshots.each do |pg|
+  puts "---------------Iteration---------------------"
+  Marshal.load(pg).cells.each { |cell| puts "[#{cell.x}, #{cell.y}], alive = #{cell.alive}" }
 end
