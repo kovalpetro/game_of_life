250c62f0ccaf7cf4c2bd04ffb080df525aa9d981
Petro Koval <kovalpetro@outlook.com>
2016-03-22 17:16:52 +0200

    Present of web (need to correct it)

--- play_web.rb
+++ play_web.rb
@@ -9,9 +9,12 @@ end
 
 post "/" do
   life_cycle = LifeCycle.new(CsvLoader.load(params[:seed]), params[:size].to_i, params[:iterations].to_i)
-  array = life_cycle.start.map do |elem|
+  @array = life_cycle.start.map do |elem|
     MatrixRepresentation.new(Marshal.load(elem).cells, params[:size].to_i).prepare
   end
-  "Data: size => #{life_cycle.size}, iterations => #{life_cycle.iterations}, seed => #{life_cycle.seed}"
-  "#{array}"
+
+  erb :representation
 end
+
+
+# @array.each {|playground| playground.each { |row| row each { |element| "!!! #{element} !!!" } } }
