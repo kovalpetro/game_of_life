ccbc4d1a8e1b1c36422db9047509476a89f5df88
Petro Koval <kovalpetro@outlook.com>
2016-03-24 11:47:32 +0200

    Move main logic from post call to support file

---
+++ lib/support/web_generate_gif.rb
@@ -0,0 +1,38 @@
+require "rmagick"
+
+class WebGenerateGif
+  attr_reader :seed, :size, :iterations
+
+  def initialize(seed, size, iterations)
+    @seed = seed
+    @size = size.to_i
+    @iterations = iterations.to_i
+  end
+
+  def generate
+    write_files_from_snapshots
+    generated_files = Dir["*.png"]
+
+    result = Magick::ImageList.new(*generated_files)
+    result.delay = 20
+    result.write("public/result.gif")
+    File.delete(*generated_files)
+  end
+
+  def write_files_from_snapshots
+    LifeCycle.new(seed, size, iterations).start.map.with_index do |elem, index|
+      data = ::MatrixWebRepresentation.new(Marshal.load(elem).cells, size).prepare
+
+      img = Magick::Image.new(size, size)
+
+      data.each_with_index do |row, row_index|
+        row.each_with_index do |item, column_index|
+          img.pixel_color(row_index, column_index, "rgb(#{item.join(', ')})")
+        end
+      end
+
+      img.write("#{index}.png")
+    end
+  end
+end
+
--- play_web.rb
+++ play_web.rb
@@ -1,36 +1,16 @@
-Dir[File.dirname(__FILE__) + "/lib/*.rb"].each { |file| require file }
 Dir[File.dirname(__FILE__) + "/lib/**/*.rb"].each { |file| require file }
 require "sinatra"
-require "byebug"
-require "rmagick"
 
 get "/" do
   erb :form
 end
 
 post "/" do
-  life_cycle = LifeCycle.new(CsvLoader.load(params[:seed]), params[:size].to_i, params[:iterations].to_i)
-  # byebug
-  life_cycle.start.map.with_index do |elem, index|
-    data = MatrixWebRepresentation.new(Marshal.load(elem).cells, params[:size].to_i).prepare
-
-    img = Magick::Image.new(params[:size].to_i, params[:size].to_i)
-
-    data.each_with_index do |row, row_index|
-      row.each_with_index do |item, column_index|
-        img.pixel_color(row_index, column_index, "rgb(#{item.join(', ')})")
-      end
-    end
-
-    img.write("#{index}.png")
-  end
-
-  generated_files = Dir["*.png"]
-
-  result = Magick::ImageList.new(*generated_files)
-  result.delay = 20
-  result.write("public/result.gif")
-  File.delete(*generated_files)
+  WebGenerateGif.new(
+    CsvLoader.load(params[:seed]),
+    params[:size],
+    params[:iterations]
+  ).generate
 
   erb :representation
 end
